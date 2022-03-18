77b9d5346931e08f3e86fc126df39313720d34c3
ShanmukhTalwar <61963603+ShanmukhTalwar@users.noreply.github.com>
2022-03-18 15:05:00 +0530

    Add files via upload

---
+++ Online-Quiz/js/quiz.js
@@ -0,0 +1,54 @@
+let quiz=[
+    {
+        question:"What is 11 x 11?",
+        option:[
+            "1) 131",
+            "2) 111",
+            "3) 121",
+            "4) 123",
+        ],
+        answer:3,
+    },
+    {
+        question:"What is 18 divided by 3?",
+        option:[
+            "1) 8",
+            "2) 9",
+            "3) 2",
+            "4) 6",
+            
+        ],
+        answer:4,
+    },
+    {
+        question:"What is 99 minus 35?",
+        option:[
+            "1) 60",
+            "2) 64",
+            "3) 89",
+            "4) 88",   
+        ],
+        answer:2,
+    },
+    {
+        question:"What is the square root of 64?",
+        option:[
+            '1) 8',
+            '2) 9',
+            '3) 7',
+            '4) 6',   
+        ],
+        answer:1,
+
+    },
+    {
+        question:"What's pi?",
+        option:[
+            "1) 3.9",
+            "2) 3",
+            "3) 3.14",
+            "4) 2.14",   
+        ],
+        answer:3,
+    }
+]
